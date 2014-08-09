c3d1964672e20310824f26733decaabe3cb592af
Rolf Johansson <rojo@nocrew.org>
2014-08-10 00:09:32 +0200

    Confirm remove
    Add pre-value to enable user confirmation before removing link to media
    item.

---
+++ ExtendedMediaPicker/extendedmediapicker.dialog.controller.js
@@ -0,0 +1,8 @@
+angular.module("umbraco")
+    .controller("CTH.ExtendedMediaPickerDialogController",
+        function ($scope) {
+
+            var dialogOptions = $scope.dialogOptions;
+            var dialogData = $scope.dialogData;
+            
+});
